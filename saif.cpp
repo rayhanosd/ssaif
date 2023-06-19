81616c1a39d106bd95282c83c936c826f63041bc
Saif Islam Rayhan <134764307+rayhanosd@users.noreply.github.com>
2023-06-19 11:59:36 +0600

    hellow word

---
+++ saif.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+    int main()
+    {
+        int num1, num2,sum;
+        num1=5;
+        num2=9;
+        sum= num1+num2;
+
+        cout<<"Sum is :"<<sum;
+    return 0;
+    }
